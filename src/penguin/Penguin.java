52501222b32eaa74202601727e511afc4a1ce4da
gc_ua4 <gc_ua4@SE137LAB-25.ad.svsu.edu>
2018-06-28 10:20:14 -0400

    first commit

---
+++ src/penguin/Penguin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penguin;
+
+/**
+ *
+ * @author gc_ua4
+ */
+public class Penguin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
