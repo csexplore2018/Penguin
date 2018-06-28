26be79b130bcd512f39a91b14404fdd55cbb8b9a
gc_ua4 <gc_ua4@SE137LAB-25.ad.svsu.edu>
2018-06-28 10:45:10 -0400

    Second commit - two classes

--- src/penguin/Penguin.java
+++ src/penguin/Penguin.java
@@ -10,12 +10,40 @@ package penguin;
  * @author gc_ua4
  */
 public class Penguin {
+    
+    private String name = "Penguin";
+    public String food = "Krill";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Animal a = new Animal();
+        System.out.println(a.name);
+        a.talk();
+ 
+        Chick b = new Chick();
+        //System.out.println(b.name);
+        b.talk();
+       
     }
     
 }
+
+class Animal{
+    
+    private String name = "Penguin";
+    public String food = "Krill";
+    public void talk(){
+        System.out.println("Squak");
+    
+    }
+    
+}
+
+class Chick extends Animal{
+    public void talk(){
+        System.out.println("Squaking");
+    }
+ }
